5c697aa246e6d2111bfbba00e55adff877f59a54
Andrew Farah <7025farah@gmail.com>
2026-05-28 09:33:15 -0700

    Show settings nav as icon only

--- mac-app/src/components/ClipboardHistory.tsx
+++ mac-app/src/components/ClipboardHistory.tsx
@@ -4742,7 +4742,7 @@ function ClipboardHistoryApp({ initialLibraryOpenTarget = null }: { initialLibra
             onClick={() => { if (!showSettings) setShowSettings(true); }}
             tabIndex={0}
             style={{
-              padding: '5px 6px',
+              padding: '5px',
               fontSize: '9px',
               fontWeight: 500,
               backgroundColor: showSettings ? theme.accent : 'transparent',
@@ -4754,7 +4754,7 @@ function ClipboardHistoryApp({ initialLibraryOpenTarget = null }: { initialLibra
               outline: 'none',
               display: 'flex',
               alignItems: 'center',
-              gap: '3px',
+              justifyContent: 'center',
               ...FIELD_THEORY_TOP_CHROME_NO_DRAG_STYLE,
             }}
             onMouseEnter={(e) => {
@@ -4773,7 +4773,6 @@ function ClipboardHistoryApp({ initialLibraryOpenTarget = null }: { initialLibra
               <circle cx="12" cy="12" r="3" />
               <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z" />
             </svg>
-            Settings
           </button>
 
           </div>
